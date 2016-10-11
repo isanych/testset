acee7f08174f072a3f9d55eb057a8f37b2e6a305
Igor Kostenko <igor_kostenko@programmingresearch.com>
2016-10-11 11:44:11 +0100

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# testset
-Test different sets performance
+# Test different sets performance
+std::set, std::unordered_set, btree::btree_set (https://code.google.com/archive/p/cpp-btree/), google::sparse_hash_set & google::dense_hash_set (https://github.com/sparsehash/sparsehash)
+
+Visual Studio 2015 update 3, 64 bit binary, 10,000,000 of random int64_t.
+
+### Memory usage (working set / size of data):
+
+set | ratio
+--- | ---
+btree::btree_set | 1.45
+google::sparse_hash_set | 1.71
+google::dense_hash_set | 5.10
+std::set | 6.22
+std::unordered_set | 7.44
+
+### Population with unique randonly distributed values (Time / Best Time):
+set | ratio
+--- | ---
+google::dense_hash_set | 1.00
+btree::btree_set | 1.86
+std::unordered_set | 2.94
+google::sparse_hash_set | 3.24
+std::set | 7.39
+
+### Search with unique randonly distributed values (Time / Best Time):
+set | ratio
+--- | ---
+google::dense_hash_set | 1.00
+std::unordered_set | 1.50
+google::sparse_hash_set | 1.71
+btree::btree_set | 2.46
+std::set | 8.98
---
+++ TestSet.cpp
@@ -0,0 +1,146 @@
+#include <random>
+#include <unordered_set>
+#include <iostream>
+#include <set>
+#include <iomanip>
+#include "btree_set.h"
+#include "sparsehash/sparse_hash_set"
+#include "sparsehash/dense_hash_set"
+
+#ifdef _DEBUG
+const auto amount = 100000;
+#else
+const auto amount = 10000000;
+#endif
+const auto mem_offset = sizeof(size_t);
+
+int64_t mem_counter = 0;
+int64_t new_counter = 0;
+int64_t del_counter = 0;
+
+int64_t reset()
+{
+	auto ret = mem_counter;
+	mem_counter = 0;
+	new_counter = 0;
+	del_counter = 0;
+	return ret;
+}
+
+#ifdef WRAP_ALLOC
+void* operator new(size_t sz) {
+	mem_counter += sz;
+	++new_counter;
+	auto ret = static_cast<size_t*>(malloc(sz + mem_offset));
+	*ret = sz;
+	return ret + 1;
+}
+
+void operator delete(void* ptr) noexcept
+{
+	auto sptr = static_cast<size_t *>(ptr) - 1;
+	mem_counter -= *sptr;
+	++del_counter;
+	free(sptr);
+}
+#endif
+
+size_t populate_count = amount;
+size_t multiplier = 0;
+
+template<typename T>
+void init_set(T& s){}
+
+template<>
+void init_set(google::dense_hash_set<int64_t>& s)
+{
+	s.set_empty_key(0);
+}
+
+template<typename T>
+void test(const char* name)
+{
+	std::uniform_int_distribution<int64_t> rnd(std::numeric_limits<int64_t>::min(), std::numeric_limits<int64_t>::max());
+	T s;
+	init_set(s);
+	{
+		std::default_random_engine generator(5489);
+		reset();
+		for (auto n = 0; n < populate_count; ++n)
+		{
+			s.insert(rnd(generator));
+		}
+	}
+	auto nc = new_counter;
+	auto dc = del_counter;
+	auto memory_size = reset();
+	auto data_size = s.size() * sizeof(int64_t);
+	std::cout << name << ": " << data_size / 1024 << " KB of data " << memory_size / 1024 << " KB allocated, ratio " << std::setprecision(2) << static_cast<double>(memory_size) / data_size << " new cnt: " << nc << " del cnt: "<< dc <<std::endl;
+	size_t cnt = 0;
+	{
+		std::default_random_engine generator(5489);
+		for (auto n = 0; n < populate_count * multiplier; ++n)
+		{
+			cnt += s.find(rnd(generator)) != s.end() ? 1 : 0;
+		}
+	}
+	std::cout << cnt << std::endl;
+}
+
+int main(int argc, char** argv)
+{
+	try
+	{
+		if (argc == 0)
+		{
+			std::cout << "Usage: test [<cnt>...] <type> " << std::endl;
+			std::cout << " s - std::set" << std::endl;
+			std::cout << " u - std::unordered_set" << std::endl;
+			std::cout << " b - btree::btree_set" << std::endl;
+			std::cout << " p - std::set" << std::endl;
+			std::cout << " s - std::set" << std::endl;
+			std::cout << " s - std::set" << std::endl;
+		}
+		for (int i = 1; i < argc; ++i)
+		{
+			std::string s(argv[i]);
+			if (s == "s")
+			{
+				test<std::set<int64_t>>("set");
+			}
+			else if (s == "u")
+			{
+				test<std::unordered_set<int64_t>>("unordered_set");
+			}
+			else if (s == "b")
+			{
+				test<btree::btree_set<int64_t>>("btree_set");
+			}
+			else if (s == "p")
+			{
+				test<google::sparse_hash_set<int64_t>>("sparse");
+			}
+			else if (s == "d")
+			{
+				test<google::dense_hash_set<int64_t>>("dense");
+			}
+			else
+			{
+				auto n = std::stoull(s);
+				if (n < 1000)
+				{
+					multiplier = n;
+				}
+				else
+				{
+					populate_count = n;
+				}
+			}
+		}
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return 0;
+}
