bf4c0c22d4500cdd65360129c5e5f7110639eac2
Igor Kostenko <igor_kostenko@programmingresearch.com>
2017-04-24 13:42:45 +0100

    Tests results

--- README.md
+++ README.md
@@ -1,32 +1,56 @@
 # Test different sets performance
 std::set, std::unordered_set, btree::btree_set (https://code.google.com/archive/p/cpp-btree/), google::sparse_hash_set & google::dense_hash_set (https://github.com/sparsehash/sparsehash)
 
-Visual Studio 2015 update 3, 64 bit binary, 10,000,000 of random int64_t.
+Visual Studio 2015 update 3, 64 bit binary, 20,000,000 of unique randomly distributed uint64_t values (153MB of data) on i7-6700 CPU.
 
-### Memory usage (process working set / size of data):
+### Memory usage (allocated memory / size of data):
 
 set | ratio
 --- | ---
-btree::btree_set | 1.45
-google::sparse_hash_set | 1.71
-google::dense_hash_set | 5.10
-std::set | 6.22
-std::unordered_set | 7.44
+btree::btree_set | 1.31
+google::sparse_hash_set | 2.31
+std::set | 5.00
+std::set pool | 5.01
+google::dense_hash_set | 6.29
+mct::closed_hash_set | 6.50
+mct::forward_hash_set | 6.50
+mct::huge_forward_hash_set | 6.50
+std::unordered_set | 9.29
+std::unordered_set pool | 9.71
+mct::huge_linked_hash_set | 9.86
 
-### Population with unique randomly distributed values (Time / Best Time):
-set | ratio
---- | ---
-google::dense_hash_set | 1.00
-btree::btree_set | 1.86
-std::unordered_set | 2.94
-google::sparse_hash_set | 3.24
-std::set | 7.39
-
-### Search with unique randomly distributed values (Time / Best Time):
+### Add new value to set (Time / Best Time):
 set | ratio
 --- | ---
 google::dense_hash_set | 1.00
-std::unordered_set | 1.50
-google::sparse_hash_set | 1.71
-btree::btree_set | 2.46
-std::set | 8.98
+mct::closed_hash_set | 1.03
+mct::huge_forward_hash_set | 1.88
+mct::forward_hash_set | 1.89
+mct::huge_linked_hash_set | 2.12
+btree::btree_set | 2.32
+std::unordered_set pool | 2.58
+std::unordered_set | 2.88
+google::sparse_hash_set | 3.53
+std::set pool | 7.63
+std::set | 8.22
+
+### Add existing value to set (Time / Best Time):
+set | ratio
+--- | ---
+google::dense_hash_set | 1.00
+mct::forward_hash_set | 1.04
+mct::huge_forward_hash_set | 1.06
+mct::closed_hash_set | 1.09
+mct::huge_linked_hash_set | 1.12
+google::sparse_hash_set | 1.72
+std::unordered_set pool | 2.03
+std::unordered_set | 2.03
+btree::btree_set | 2.66
+std::set pool | 10.55
+std::set | 10.55
+
+There is no advantage in usage of `std::set` or `std::unordered_set` unless your data set is very small. `std::set` is very slow, `std::unordered_set` is not the fastest and uses too much memory (and memory pool does not help them much).<BR>
+`google::dense_hash_set` is the fastest implementation but it requires you to set aside one key value.<BR>
+`mct::closed_hash_set` is close to `google::dense_hash_set` by performance and memory consumption and does not have limitations on values in set.<BR>
+`btree::btree_set` has very small memory overhead and decent performance in comparison with `std::unordered_set`, but slower than fastest implementations.<BR>
+I’ve tried to use compressed & sparse bitsets, but have not found implementations which could handle full range of 64 bit values and even on smaller ranges all implementations which I’ve tried have performance issues with multiple inserts.
---
+++ results.py
@@ -0,0 +1,119 @@
+from __future__ import print_function
+import sys
+
+
+def same_or_none(old, new):
+    if old and old != new:
+        raise ValueError(str(old) + " != " + str(new))
+    return new
+
+
+def smaller_non_zero(old, new):
+    return new if new > 0 and (old is None or old > new) else old
+
+
+def str_or_empty(s):
+    return "" if s is None else str(s)
+
+
+class Mem:
+    def __init__(self, cnt=None, size=None):
+        self.cnt = cnt
+        self.size = size
+
+    def same_or_none(self, cnt, size):
+        self.cnt = same_or_none(self.cnt, cnt)
+        self.size = same_or_none(self.size, size)
+
+    def __str__(self):
+        return str_or_empty(self.cnt) + "," + str_or_empty(self.size)
+
+
+class Test:
+    def __init__(self, name):
+        self.name = name
+        self.number_of_runs = 0
+        self.alloc = Mem()
+        self.free = Mem()
+        self.grow = Mem()
+        self.shrink = Mem()
+        self.population = None
+        self.population_hit = None
+        self.hit = None
+        self.miss = None
+        self.working_set = None
+
+    def __str__(self):
+        l = list()
+        l.append(self.name)
+        l.append(str(self.alloc))
+        l.append(str(self.free))
+        l.append(str(self.grow))
+        l.append(str(self.shrink))
+        l.append(str_or_empty(self.population))
+        l.append(str_or_empty(self.population_hit))
+        l.append(str_or_empty(self.hit))
+        l.append(str_or_empty(self.miss))
+        l.append(str_or_empty(self.working_set))
+        return ",".join(l)
+
+
+tests = dict()
+cnt = None
+data_size = None
+is_pool = False
+
+with open(sys.argv[1]) as f:
+    for line in f:
+        s = line.split()
+        if not s:
+            continue
+        if s[0] == "Testing:":
+            is_wrap_alloc = "wrap_alloc" in s
+            is_pool = "pool" in s
+            name = s[1] + (" pool" if is_pool else "")
+            cnt = same_or_none(cnt, int(s[7]))
+            if name in tests:
+                test = tests[name]
+            else:
+                test = Test(name)
+                tests[name] = test
+            test.number_of_runs += 1
+            continue
+        if line.startswith("Data size: "):
+            data_size = same_or_none(data_size, int(s[2]))
+            continue
+        if s[0] == "Exit":
+            if s[3] != "0":
+                raise ValueError("Exit code " + s[3])
+            continue
+        if s[0] == "population,":
+            test.population = smaller_non_zero(test.population, float(s[2]))
+            continue
+        if s[0] == "population":
+            test.population_hit = smaller_non_zero(test.population_hit, float(s[3]))
+            continue
+        if s[0] == "hit,":
+            test.hit = smaller_non_zero(test.hit, float(s[2]))
+            continue
+        if s[0] == "miss,":
+            test.miss = smaller_non_zero(test.miss, float(s[2]))
+            continue
+        if s[0] == "Working":
+            test.working_set = smaller_non_zero(test.working_set, int(s[3]))
+            continue
+        if is_pool:
+            if s[0] == "New:":
+                test.alloc.same_or_none(int(s[1]), int(s[3]))
+        else:
+            if s[0] == "Alloc:":
+                test.alloc.same_or_none(int(s[1]), int(s[3]))
+            elif s[0] == "Free:":
+                test.free.same_or_none(int(s[1]), int(s[3]))
+            elif s[0] == "Grow:":
+                test.grow.same_or_none(int(s[1]), int(s[3]))
+            elif s[0] == "Shrink:":
+                test.shrink.same_or_none(int(s[1]), int(s[3]))
+print(str(cnt) + "," + str(data_size))
+for test in tests.values():
+    print(test)
